ca47edf3a7d0e230b226bceee7d0b520f3e541b4
dannielarenderos <00087717@uca.edu.sv>
2020-04-25 00:34:10 -0600

    commit

---
+++ src/main/java/com/uca/capas/modelo/controller/Clase10Controller.java
@@ -0,0 +1,82 @@
+package com.uca.capas.modelo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class Clase10Controller {
+	
+	@RequestMapping("/ingresar")
+	public String index() {
+		/* No es necesario poner la extension .html a index, ya que el metodo
+		 * templateResolver.setSuffix(".html") sirve para asumir dicha extension.
+		 * Como solo vamos a redirigir a la pagina index.html no es necesario un 
+		 * objeto ModelAndView, al devolver un tipo de dato String, Spring automaticamente
+		 * asume que es una vista la que se quiere devolver.
+		 */
+		return "clases/clase10/index";
+	}
+	
+	@RequestMapping("/parametros1")
+	public ModelAndView parametros1(HttpServletRequest request) {
+		ModelAndView mav = new ModelAndView();
+		String usuario = request.getParameter("usuario");
+		String apellido = request.getParameter("apellido");
+		String fecha = request.getParameter("fecha");
+		String lugar = request.getParameter("lugar");
+		String colegio = request.getParameter("colegio");
+		String tel = request.getParameter("tel");
+		String cel = request.getParameter("cel");
+
+		
+		List<String> lista = new ArrayList<>();
+		String mensaje= "Alumno ingresado con éxito";
+		
+		
+		if(usuario.length()<1 || usuario.length()>25) {
+			lista.add("“El campo Nombres no puede ser mayor a 25 caracteres o menor a 1 caracter");
+		}
+		
+		if(apellido.length()<1 || apellido.length()>25) {
+			lista.add("“El campo Apellido no puede ser mayor a 25 caracteres o menor a 1 caracter");
+		}
+		
+		if(lugar.length()<1 || lugar.length()>25) {
+			lista.add("“El campo Lugar no puede ser mayor a 25 caracteres o menor a 1 caracter");
+		}
+		
+		if(tel.length()!=8) {
+			lista.add("“El campo Telefono debe tener 8 numeros");
+		}
+		
+
+		if(cel.length()!=8) {
+			lista.add("“El campo Celular debe tener 8 numeros");
+		}
+		
+		if(colegio.length()<1 || colegio.length()>100) {
+			lista.add("“El campo Colegio no puede ser mayor a 100 caracteres o menor a 1 caracter");
+		}
+		
+		if(lista.isEmpty()) {
+			mav.addObject("mensaje", mensaje);
+			mav.setViewName("clases/clase10/resultado2");
+
+		}else {
+			mav.addObject("errores", lista);
+			mav.setViewName("clases/clase10/resultado");
+		}
+		
+		
+		return mav;
+		
+	}
+	
+	
+}
